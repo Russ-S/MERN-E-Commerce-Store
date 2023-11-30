f2499b2bb096f841c9abfc32c7270ffe4382a363
Russ-S <hesys@cs.com>
2023-11-30 16:19:03 -0700

    First commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,38 @@
+import express from "express";
+import formidable from "express-formidable";
+const router = express.Router();
+
+// controllers
+import {
+  addProduct,
+  updateProductDetails,
+  removeProduct,
+  fetchProducts,
+  fetchProductById,
+  fetchAllProducts,
+  addProductReview,
+  fetchTopProducts,
+  fetchNewProducts,
+} from "../controllers/productController.js";
+
+import { protect, admin } from "../middlewares/authMiddleware.js";
+import checkId from "../middlewares/checkId.js";
+
+router
+  .route("/")
+  .get(fetchProducts)
+  .post(protect, admin, formidable(), addProduct);
+
+router.route("/allproducts").get(fetchAllProducts);
+router.route("/:id/reviews").post(protect, checkId, addProductReview);
+
+router.get("/top", fetchTopProducts);
+router.get("/new", fetchNewProducts);
+
+router
+  .route("/:id")
+  .get(fetchProductById)
+  .put(protect, admin, formidable(), updateProductDetails)
+  .delete(protect, admin, removeProduct);
+
+export default router;
---
+++ frontend/src/main.jsx
@@ -0,0 +1,59 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Route, RouterProvider, createRoutesFromElements } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store.js";
+
+// Private Route
+import PrivateRoute from "./components/PrivateRoute.jsx";
+
+// Admin Route
+import AdminRoute from "./pages/Admin/AdminRoute.jsx";
+
+// Auth
+import Login from "./pages/Auth/Login.jsx";
+import Register from "./pages/Auth/Register.jsx";
+
+import Profile from "./pages/User/Profile.jsx";
+import UserList from "./pages/Admin/UserList.jsx";
+import CategoryList from "./pages/Admin/CategoryList.jsx";
+import ProductList from "./pages/Admin/ProductList.jsx";
+import AllProducts from "./pages/Admin/AllProducts.jsx";
+import ProductUpdate from "./pages/Admin/ProductUpdate.jsx";
+import Home from "./Home.jsx";
+
+import Favorites from "./pages/Products/Favorites.jsx";
+import ProductDetails from "./pages/Products/ProductDetails.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/favorite" element={<Favorites />} />
+      <Route path="/product/:id" element={<ProductDetails />} />
+
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
+
+      {/* Admin Routes */}
+      <Route path="/admin" element={<AdminRoute />}>
+        <Route path="userlist" element={<UserList />} />
+        <Route path="categorylist" element={<CategoryList />} />
+        <Route path="productlist" element={<ProductList />} />
+        <Route path="allproductslist" element={<AllProducts />} />
+        <Route path="product/update/:_id" element={<ProductUpdate />} />
+      </Route>
+    </Route>
+  )
+);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+);
